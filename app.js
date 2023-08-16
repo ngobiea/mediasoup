e85d14fb20aa42c164f30e77c3f4db4163d8e99a
ngobiea <70290199+ngobiea@users.noreply.github.com>
2023-08-16 19:01:56 +0500

    introduction to media soup

---
+++ app.js
@@ -0,0 +1,260 @@
+import express from 'express';
+const app = express();
+const PORT = 5000;
+
+import http from 'http';
+
+import path from 'path';
+const __dirname = path.resolve();
+
+import { Server } from 'socket.io';
+import mediasoup from 'mediasoup';
+
+app.get('/', (_req, res) => {
+  res.send('Hello from mediasoup app!');
+});
+
+app.use('/sfu', express.static(path.join(__dirname, 'public')));
+
+
+
+const httpsServer = http.createServer(app);
+httpsServer.listen(PORT, () => {
+  console.log('listening on port: ' + PORT);
+});
+
+const io = new Server(httpsServer);
+
+// socket.io namespace (could represent a room?)
+const peers = io.of('/mediasoup');
+
+/**
+ * Worker
+ * |-> Router(s)
+ *     |-> Producer Transport(s)
+ *         |-> Producer
+ *     |-> Consumer Transport(s)
+ *         |-> Consumer
+ **/
+let worker;
+let router;
+let producerTransport;
+let consumerTransport;
+let producer;
+let consumer;
+
+// mediasoup server
+const createWorker = async () => {
+  const exitTimeout = 2000;
+  worker = await mediasoup.createWorker({
+    rtcMinPort: 2000,
+    rtcMaxPort: 2020,
+  });
+  console.log(`worker pid: ${worker.pid}`);
+  worker.on('died', () => {
+    console.log('mediasoup worker died, exiting in 2 seconds...');
+    console.error('mediasoup worker has died');
+    setTimeout(() => process.exit(1), exitTimeout);
+  });
+  return worker;
+};
+
+worker = createWorker();
+
+const mediaCodecs = [
+  {
+    kind: 'audio',
+    mimeType: 'audio/opus',
+    clockRate: 48000,
+    channels: 2,
+  },
+  {
+    kind: 'video',
+    mimeType: 'video/VP8',
+    clockRate: 90000,
+    parameters: {
+      'x-google-start-bitrate': 1000,
+    },
+  },
+];
+
+peers.on('connection', async (socket) => {
+  console.log(socket.id);
+  socket.emit('connection-success', { socketId: socket.id });
+  socket.on('disconnect', () => {
+    // do some cleanup
+    console.log('peer disconnected');
+  });
+
+  // worker.createRouter(options)
+  // options = { mediaCodecs, appData }
+  // mediaCodecs -> defined above
+  // appData -> custom application data - we are not supplying any
+  // none of the two are required
+  router = await worker.createRouter({ mediaCodecs });
+
+  // Client emits a request for RTP Capabilities
+  // This event responds to the request
+  socket.on('getRtpCapabilities', (callback) => {
+    const rtpCapabilities = router.rtpCapabilities;
+
+    console.log('rtp Capabilities', rtpCapabilities);
+
+    // call callback from the client and send back the rtpCapabilities
+    callback({ rtpCapabilities });
+  });
+
+  // Client emits a request to create server side Transport
+  // We need to differentiate between the producer and consumer transports
+  socket.on('createWebRtcTransport', async ({ sender }, callback) => {
+    console.log(`Is this a sender request? ${sender}`);
+    // The client indicates if it is a producer or a consumer
+    // if sender is true, indicates a producer else a consumer
+    if (sender) {
+      producerTransport = await createWebRtcTransport(callback);
+    } else {
+      consumerTransport = await createWebRtcTransport(callback);
+    }
+  });
+
+  // see client's socket.emit('transport-connect', ...)
+  socket.on('transport-connect', async ({ dtlsParameters }) => {
+    console.log('DTLS PARAMS... ', { dtlsParameters });
+    await producerTransport.connect({ dtlsParameters });
+  });
+
+  // see client's socket.emit('transport-produce', ...)
+  socket.on(
+    'transport-produce',
+    async ({ kind, rtpParameters, _appData }, callback) => {
+      // call produce based on the parameters from the client
+      producer = await producerTransport.produce({
+        kind,
+        rtpParameters,
+      });
+
+      console.log('Producer ID: ', producer.id, producer.kind);
+
+      producer.on('transportclose', () => {
+        console.log('transport for this producer closed ');
+        producer.close();
+      });
+
+      // Send back to the client the Producer's id
+      callback({
+        id: producer.id,
+      });
+    }
+  );
+
+  // see client's socket.emit('transport-recv-connect', ...)
+  socket.on('transport-recv-connect', async ({ dtlsParameters }) => {
+    console.log(`DTLS PARAMS: ${dtlsParameters}`);
+    await consumerTransport.connect({ dtlsParameters });
+  });
+
+  socket.on('consume', async ({ rtpCapabilities }, callback) => {
+    try {
+      // check if the router can consume the specified producer
+      if (
+        router.canConsume({
+          producerId: producer.id,
+          rtpCapabilities,
+        })
+      ) {
+        // transport can now consume and return a consumer
+        consumer = await consumerTransport.consume({
+          producerId: producer.id,
+          paused: true,
+          rtpCapabilities,
+        });
+
+        consumer.on('transportclose', () => {
+          console.log('transport close from consumer');
+        });
+
+        consumer.on('producerclose', () => {
+          console.log('producer of consumer closed');
+        });
+
+        // from the consumer extract the following params
+        // to send back to the Client
+        const params = {
+          id: consumer.id,
+          producerId: producer.id,
+          kind: consumer.kind,
+          rtpParameters: consumer.rtpParameters,
+        };
+
+        // send the parameters to the client
+        callback({ params });
+      }
+    } catch (error) {
+      console.log(error.message);
+      callback({
+        params: {
+          error,
+        },
+      });
+    }
+  });
+
+  socket.on('consumer-resume', async () => {
+    console.log('consumer resume');
+    await consumer.resume();
+  });
+});
+
+const createWebRtcTransport = async (callback) => {
+  try {
+    // https://mediasoup.org/documentation/v3/mediasoup/api/#WebRtcTransportOptions
+    const webRtcTransport_options = {
+      listenIps: [
+        {
+          // replace with relevant IP address
+          ip: '0.0.0.0',
+          announcedIp: '127.0.0.1',
+        },
+      ],
+      enableUdp: true,
+      enableTcp: true,
+      preferUdp: true,
+    };
+
+    // https://mediasoup.org/documentation/v3/mediasoup/api/#router-createWebRtcTransport
+    const transport = await router.createWebRtcTransport(
+      webRtcTransport_options
+    );
+    console.log(`transport id: ${transport.id}`);
+
+    transport.on('dtlsstatechange', (dtlsState) => {
+      if (dtlsState === 'closed') {
+        transport.close();
+      }
+    });
+
+    transport.on('close', () => {
+      console.log('transport closed');
+    });
+
+    // send back to the client the following parameters
+    callback({
+      // https://mediasoup.org/documentation/v3/mediasoup-client/api/#TransportOptions
+      params: {
+        id: transport.id,
+        iceParameters: transport.iceParameters,
+        iceCandidates: transport.iceCandidates,
+        dtlsParameters: transport.dtlsParameters,
+      },
+    });
+
+    return transport;
+  } catch (error) {
+    console.log(error);
+    callback({
+      params: {
+        error,
+      },
+    });
+  }
+};
